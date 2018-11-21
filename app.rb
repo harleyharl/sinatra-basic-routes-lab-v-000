c2df335dccfab4274eaed5ee6a524750623d7d20
Harley Rayner <harleymteden@gmail.com>
2018-11-21 00:33:05 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,17 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  
+
   get '/name' do
   "Harley"
   end
 
-  get '/hometown'
+  get '/hometown' do
   "Auckland"
   end
 
-  get '/favorite-song'
+  get '/favorite-song' do
   "sick song"
-  end 
-  
+  end
+
 end
